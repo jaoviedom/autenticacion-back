ab4359c647737f93badd5c7f0abe786acebc6c33
jaoviedom <guitaroviedo46@gmail.com>
2023-04-05 08:53:11 -0500

    Full

--- base/api/urls.py
+++ base/api/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('', views.getRoutes),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('notes/', views.getNotes),
 ]
--- base/api/views.py
+++ base/api/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import NoteSerializer
+from base.models import Note
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -25,3 +29,12 @@ def getRoutes(request):
         '/api/token/refresh',
     ]
     return Response(routes)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    user = request.user
+    # notes = Note.objects.all()
+    notes = user.note_set.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
